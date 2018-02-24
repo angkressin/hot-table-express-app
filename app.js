521d33b16f21e3209c867f53477f54889d91dee9
dbailey25 <dtbailey25@gmail.com>
2018-02-24 11:34:08 -0500

    adding html routes

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// paths to html pages
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+
+});
+
+app.get("/reservations", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+
+
+
+
+
+
+
+
 
 // Starts the server to begin listening
 // =============================================================
