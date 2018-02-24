c48c86a691c92ac5f9eb15631fc43f9eeadf6bbc
michael bychkowski <mbychkowski@gmail.com>
2018-02-24 13:19:49 -0500

    small fixes, formatting, postman testing worked

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var waitlist = [];
-var reservation = [];
+var reservations = [];
+
+// TEST Table
+var reservations = [{
+  name: "Yoda",
+  phone: "222-222-2222",
+  email: "email-yoda@gmai.com",
+  id: 1
+}];
 
 // Sets up the Express App
 // =============================================================
@@ -13,46 +21,37 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // paths to html pages
-
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
-
 });
 
 app.get("/view", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
-
 });
 
 app.get("/reservations", function(req, res) {
   res.sendFile(path.join(__dirname, "reservations.html"));
 });
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
-
-
-
-if (reservation.length > 5) {
+if (reservations.length > 5) {
   // post to waitlist
   app.post('/api/waitlist', function(req, res) {
-    processRequest(waitlist);
+    processRequest(req, res, waitlist);
   });
 } else {
   // post to reseravtion
-  app.post('/api/reseravtion', function(req, res) {
-    processRequest(reservation);
+  app.post('/api/reservations', function(req, res) {
+    processRequest(req, res, reservations);
   });
 }
 
-function processRequest(pushArray) {
+function processRequest(req, res, pushArray) {
   var newRequest = req.body;
 
   newRequest.routeName = newRequest.name.replace(/\s+/g, "").toLowerCase();
@@ -63,3 +62,9 @@ function processRequest(pushArray) {
 
   res.json(newRequest);
 }
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
